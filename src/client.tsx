5bbe90041201c5f518bd2ea3c924f1bd8151a933
Tom Radford <tom@theradford.com>
2026-04-25 23:21:33 +0200

    stale chunk handling

--- src/client.tsx
+++ src/client.tsx
@@ -1,5 +1,12 @@
 import { initClient, initClientNavigation } from "rwsdk/client";
 
+// this is for new deploys that need to reload when new chunks....
+// very stange i was getting this, im pretty sure lib should handle this?!
+window.addEventListener("vite:preloadError", (event) => {
+  event.preventDefault();
+  window.location.reload();
+});
+
 const { handleResponse, onHydrated } = initClientNavigation();
 
 initClient({ handleResponse, onHydrated });

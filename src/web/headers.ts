e5daec89522846cedd1dcf8907ebff2c7c134739
Tom Radford <tom@theradford.com>
2026-04-26 18:38:16 +0200

    fix csp headers for dev

--- src/web/headers.ts
+++ src/web/headers.ts
@@ -8,7 +8,7 @@ export const setCommonHeaders =
       // Forces browsers to always use HTTPS for a specified time period (2 years)
       response.headers.set(
         "Strict-Transport-Security",
-        "max-age=63072000; includeSubDomains; preload"
+        "max-age=63072000; includeSubDomains; preload",
       );
     }
 
@@ -21,12 +21,16 @@ export const setCommonHeaders =
     // Explicitly disables access to specific browser features/APIs
     response.headers.set(
       "Permissions-Policy",
-      "geolocation=(), microphone=(), camera=()"
+      "geolocation=(), microphone=(), camera=()",
     );
 
     // Defines trusted sources for content loading and script execution:
+    const s3UrlWithTrailingSlash = env.S3_PUBLIC_ACCESS.endsWith("/")
+      ? env.S3_PUBLIC_ACCESS
+      : `${env.S3_PUBLIC_ACCESS}/`;
+
     response.headers.set(
       "Content-Security-Policy",
-      `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: *; frame-src https://challenges.cloudflare.com; connect-src 'self' *; media-src 'self' blob: data: ${env.S3_PUBLIC_ACCESS}; object-src 'none';`
+      `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: *; frame-src https://challenges.cloudflare.com; connect-src 'self' *; media-src 'self' blob: data: ${s3UrlWithTrailingSlash}; object-src 'none';`,
     );
   };
